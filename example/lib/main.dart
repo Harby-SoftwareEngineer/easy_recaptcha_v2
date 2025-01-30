21faf107fa9e495b9c1c4d0fb8407e848e9ec381
Mohamed Ben Halima <mohamedbenhalima48@gmail.com>
2025-01-30 16:56:02 +0100

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:flutter_easy_recaptcha_v2/flutter_easy_recaptcha_v2.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo flutter_easy_recaptcha_v2 ',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Demo flutter_easy_recaptcha_v2 '),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _showRecaptchaBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      isDismissible: false,
+      backgroundColor: Colors.white,
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 700,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    weight: 20,
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ),
+              SizedBox(
+                height: 600,
+                child: RecaptchaV2(
+                  // Your API Key
+                  apiKey: "your-apiKey",
+                  onVerifiedSuccessfully: (token) async {
+                    log("Recaptcha token $token");
+                    // It is recommended to verify the token on your server but you can also verify it here.
+                    final bool isTokenVerified = await verifyRecaptchaV2Token(
+                      token: token,
+                      apiSecret: "your-apiSecret",
+                    );
+                    if (isTokenVerified) {
+                      log("Token verified successfully");
+                      // Implement your logic here
+                    } else {
+                      log("Token verification failed");
+                      // Implement your logic here
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Tap on the button to show the RecaptchaV2 Screen',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        onPressed: () {
+          _showRecaptchaBottomSheet(context);
+        },
+        child: Text(
+          "Show RecaptchaV2",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/flutter_easy_recaptcha_v2.dart
@@ -0,0 +1,104 @@
+library flutter_easy_recaptcha_v2;
+
+import 'dart:convert';
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:http/http.dart' as http;
+
+InAppLocalhostServer localhostServer = InAppLocalhostServer();
+
+class RecaptchaV2 extends StatefulWidget {
+  final String apiKey;
+
+  final ValueChanged<String>? onVerifiedSuccessfully;
+
+  const RecaptchaV2({
+    super.key,
+    required this.apiKey,
+    this.onVerifiedSuccessfully,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _RecaptchaV2State();
+}
+
+class _RecaptchaV2State extends State<RecaptchaV2> {
+  bool _isLoading = true;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await localhostServer.start();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? Center(child: CircularProgressIndicator())
+        : InAppWebView(
+            initialUrlRequest: URLRequest(
+              url: WebUri.uri(
+                Uri.parse(
+                  "http://localhost:8080/packages/flutter_easy_recaptcha_v2/assets/index.html?api_key=${widget.apiKey}",
+                ),
+              ),
+            ),
+            initialSettings: InAppWebViewSettings(
+              javaScriptEnabled: true,
+              transparentBackground: true,
+            ),
+            onWebViewCreated: (controller) {},
+            onLoadStop: (controller, url) {
+              setState(() {
+                _isLoading = false;
+              });
+            },
+            onConsoleMessage: (controller, consoleMessage) {
+              log("[flutter_easy_recaptcha_v2] consoleMessage ${consoleMessage.message}");
+              if (consoleMessage.messageLevel == ConsoleMessageLevel.LOG &&
+                  // Verifying if the string is a token or not.
+                  consoleMessage.message.length > 70) {
+                String token = consoleMessage.message;
+                if (token.contains("verify")) {
+                  token = token.substring(7);
+                }
+                widget.onVerifiedSuccessfully?.call(token);
+              }
+            },
+          );
+  }
+
+  @override
+  void dispose() {
+    localhostServer.close();
+    super.dispose();
+  }
+}
+
+Future<bool> verifyRecaptchaV2Token({
+  required String token,
+  required String apiSecret,
+}) async {
+  String url = "https://www.google.com/recaptcha/api/siteverify";
+  http.Response response = await http.post(Uri.parse(url), body: {
+    "secret": apiSecret,
+    "response": token,
+  });
+
+  if (response.statusCode == 200) {
+    dynamic json = jsonDecode(response.body);
+    if (json['success']) {
+      return true;
+    } else {
+      log("[flutter_easy_recaptcha_v2] Error while verifying recaptcha token: ${json['error-codes'].toString()}");
+      return false;
+    }
+  }
+  log("[flutter_easy_recaptcha_v2] Error while verifying recaptcha token");
+  return false;
+}
